831264498b42479274b0316883230dcbdf1edd7c
Teise Fabian <dqm15teise@szut.uni-bremen.de>
2018-03-12 13:18:45 +0100

    color change, spawning and clicking on target working

--- script.js
+++ script.js
@@ -2,19 +2,23 @@ $(document).ready(function(){
    
   var gameOn = true; 
   var i = 1;
+  var spawnTimer = 1000;
   spawning();
   
   function spawning() {
-    	var t = Math.floor(Math.random() * 360) + 90; 
-      var l = Math.floor(Math.random() * 615) + 45;
+    	var t = Math.floor(Math.random() * 97) + 2; 
+      var l = Math.floor(Math.random() * 97) + 2;
       $(".bg").append("<div id='"+i+"' class='target'></div>");
-
-     $(".target#"+i).css( {"top" : t+"px", "left" : l+"px" });
-  
+      $(".target#"+i).css( {"top" : t+"%", "left" : l+"%" });
+      $(".target").click(function(){
+        $(this).hide();
+      });   
       i++;
-      setTimeout(spawning() , 1000); 
-   }
+      spawnTimer = spawnTimer - 10;
+     
   
-    
+   }
+
+   setInterval(spawning , spawnTimer); 
   
 });
