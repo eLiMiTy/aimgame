7f4871e34a4c5e3afb450fe7405e0775b9f53f70
elimity <Fabian-Teise@gmx.de>
2018-04-02 11:43:35 +0200

    Unfertiges spiel am ende des wissens

--- script.js
+++ script.js
@@ -1,24 +1,73 @@
 $(document).ready(function(){
-   
-  var gameOn = true; 
+ 
+  var j = -2;
   var i = 1;
   var spawnTimer = 1000;
-  spawning();
-  
+  var life = 3;
+ 
+  var buttonClick = false;
+  var w8 = 0;
+  var gameOver = false;
+
+ $('#button').click(function() {
+    $(this).remove();
+    spawning();
+    buttonClick = true
+  });
+
+
+  function dmg() {
+    if (life == 3) {
+      life = life - 1
+     $(".bg").text("Leben: X X ");
+      console.log("hit 1");
+    }else if (life == 2) {
+      life = life - 1
+     $(".bg").text("Leben: X ");
+       console.log("hit 2");
+    }else if (life == 1) {
+      life = life - 1
+     $(".bg").text("Leben:");
+       console.log("hit 3");
+       gameOver= true;
+    }
+  }
+
+
   function spawning() {
-    	var t = Math.floor(Math.random() * 97) + 2; 
-      var l = Math.floor(Math.random() * 97) + 2;
+    if (buttonClick == true) {
+    	var t = Math.floor(Math.random() * 94) + 2; 
+      var l = Math.floor(Math.random() * 94) + 2;
       $(".bg").append("<div id='"+i+"' class='target'></div>");
       $(".target#"+i).css( {"top" : t+"%", "left" : l+"%" });
       $(".target").click(function(){
-        $(this).hide();
+          $(this).remove();
       });   
       i++;
-      spawnTimer = spawnTimer - 10;
-     
-  
-   }
+      decay();
+      
+    }
+  }
 
-   setInterval(spawning , spawnTimer); 
+  
+  function decay() {
+      console.log("decay");
+      $(".target#"+j).remove(); 
+      j++
+     
+        dmg();  
+
+        
+
+  }
+
+  if (gameOver == true) {
+      console.log("game Over");
+  }else if(gameOver == false){
+    console.log("game is going")
+    setInterval(spawning, spawnTimer);
+  }
+
   
 });
+
